ec150904b09a32e94b4fef06168a32aa0357e9f3
caazhr <neisyaazzahra7@gmail.com>
2025-10-01 20:41:52 +0700

    Add files via upload

---
+++ MODUL 1 - Pengenalan Bahasa C++/Unguided/aritmatika1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   float a, b;
+
+   cout << "Masukkan Angka1: ";
+    cin >> a;
+    cout << "Masukkan Angka2: ";
+    cin >> b;
+
+    cout << "Hasil Penjumlahan = " << (a + b) << endl;
+    cout << "Hasil Pengurangan = " << (a - b) << endl;
+    cout << "Hasil Perkalian = " << (a * b) << endl;
+    cout << "Hasil Pembagian = " << (a / b) << endl;
+   
+
+    return 0;
+}

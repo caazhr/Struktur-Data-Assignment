be51ccaae0a1bbbb9b4446310c86cf5bbe0d3077
caazhr <neisyaazzahra7@gmail.com>
2025-10-01 19:56:05 +0700

    Add files via upload

---
+++ MODUL 1 - Pengenalan Bahasa C++/Guided/Struktur.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa {
+    string nama;
+    int umur;
+    
+};
+
+int main() {
+  int jumlah;
+
+  cout << "Masukkan jumlah mahasiswa: ";
+    cin >> jumlah;
+
+    Mahasiswa mhs[jumlah];
+
+    //input data  menggunakan loop
+    for (int i = 0; i < jumlah; i++) {
+        cout << "\nMahasiswa ke-" << i + 1 << endl;
+        cout << "Masukkan Nama: ";
+        cin >> mhs[i].nama;
+        cout << "Masukkan Umur: ";
+        cin >> mhs[i].umur;
+    }
+    //Tampilkan data
+    cout << "\n=== Data Mahasiswa ===\n";
+    for (int i = 0; i < jumlah; i++) {
+        cout << "Mahasiswa ke-" << i + 1 
+             << " | Nama: " << mhs[i].nama 
+             << " | Umur: " << mhs[i].umur << endl;
+    }
+
+    return 0;
+}

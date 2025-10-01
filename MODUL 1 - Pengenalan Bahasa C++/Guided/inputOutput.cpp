7401501db66f7481d430893630b29a287909caaa
caazhr <neisyaazzahra7@gmail.com>
2025-10-01 19:44:26 +0700

    Add files via upload

---
+++ MODUL 1 - Pengenalan Bahasa C++/Guided/inputOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std; 
+int main() {
+    int n;
+    const float phi = 3.14;
+
+    cout << "Masukkan Angka: ";
+    cin >> n;
+
+    cout << "Angka Dikeluarkan: " << n << endl;
+    cout << "Nilai Konstanta Phi: " << phi << endl;
+    return 0;
+}

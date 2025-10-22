0a09ffc9b73fbf0ac981fc6bb735a9a2bca343c7
Neisya Azzahra Rasin <neisyaazzahra7@gmail.com>
2025-10-22 20:43:08 +0700

    Add files via upload

---
+++ MODUL 4 - Singly Linked List (Bagian Pertama)/Unguided/soal 1/singlylist.cpp
@@ -0,0 +1,39 @@
+#include "Singlylist.h"
+#include <iostream>
+using namespace std;
+
+void CreateList(List &L) {
+    L.First = Nil;
+}
+
+address alokasi(infotype x) {
+    address P = new ElmtList;
+    if (P != Nil) {
+        P->info = x;
+        P->next = Nil;
+    }
+    return P;
+}
+
+void dealokasi(address &P) {
+    delete P;
+    P = Nil;
+}
+
+void printInfo(List L) {
+    if (L.First == Nil) {
+        cout << "List kosong." << endl;
+    } else {
+        address P = L.First;
+        while (P != Nil) {
+            cout << P->info << " ";
+            P = P->next;
+        }
+        cout << endl;
+    }
+}
+
+void insertFirst(List &L, address P) {
+    P->next = L.First;
+    L.First = P;
+}
